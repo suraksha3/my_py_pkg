471dea39dc50ba834ba99673463a06754e549154
suraksha3 <suraksha3devadiga@gmail.com>
2026-03-06 11:38:12 +0530

    add

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='suraksha',
+    maintainer_email='suraksha@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = my_py_pkg.pub:main',
+            'listener = my_py_pkg.sub:main',
+
+        ],
+    },
+)
